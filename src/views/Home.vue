879cdfd182b2670e3e1389cac03197617d68802a
Naveen Budhwal <naveenbudhwal@gmail.com>
2020-04-01 19:49:01 +0530

    adding components to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    
+    <h1>Some reusable components</h1>
+    <tag-input />
   </div>
 </template>
 
 <script>
+import tagInput from '@/components/tagInput.vue';
   export default {
-    
+    components: {
+      tagInput
+    }
   }
 </script>
 
