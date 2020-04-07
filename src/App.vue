700bd078cf38514fca3915d51c25cddad6a07195
Naveen Budhwal <naveenbudhwal@gmail.com>
2020-04-07 11:21:58 +0530

    Routing animations

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,60 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/">Home</router-link> | 
+      <router-link to="/side-nav">Side Nav</router-link>
     </div>
-    <router-view/>
+    <transition name="slide-fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
 <style>
+
+/* * {
+  margin: 0;
+  padding: 0;
+} */
+
+/* -------- Trasition Styles ------- */
+
+.slide-fade-enter {
+  opacity: 0;
+  transform: translateX(50px);
+}
+
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: all 0.3s ease-in-out;
+}
+
+.slide-fade-leave {
+  opacity: 0;
+  transform: translateX(-50px);
+}
+
+/* --------------------------------- */
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  font-family: Noah, sans-serif;
 }
 
 #nav {
   padding: 30px;
+  text-align: center;
 }
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  font-size: 1.2rem;
 }
 
 #nav a.router-link-exact-active {
-  color: #000;
+  color: #2b70dc;
 }
 </style>
